48e3a0593c1c5bd48082d6d9ffc1be520e206a88
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-03-23 02:32:22 +0330

    update termux.sh

--- termux.sh
+++ termux.sh
@@ -33,29 +33,6 @@ check_dependencies() {
     done
 }
 
-# Build
-build() {
-    if command -v warp &> /dev/null || command -v usef &> /dev/null; then
-        echo -e "${green}Warp is already installed.${rest}"
-        return
-    fi
-
-    echo -e "${green}Installing Warp...${rest}"
-    pkg update -y && pkg upgrade -y
-    check_dependencies_build
-
-    if git clone https://github.com/bepass-org/wireguard-go.git &&
-        cd wireguard-go &&
-        go build main.go &&
-        chmod +x main &&
-        cp main "$PREFIX/bin/usef" &&
-        cp main "$PREFIX/bin/warp"; then
-        echo -e "${green}Warp installed successfully.${rest}"
-    else
-        echo -e "${red}Error installing WireGuard VPN.${rest}"
-    fi
-}
-
 # Install
 install() {
     if command -v warp &> /dev/null || command -v usef &> /dev/null; then
@@ -68,12 +45,14 @@ install() {
     pacman -Syu openssh = apt update; apt full-upgrade -y; apt install -y openssh
     check_dependencies
 
-    if wget https://github.com/bepass-org/wireguard-go/releases/download/v1.0.4-rc2/warp-android-arm64.b6d64e.zip &&
-        unzip warp-android-arm64.b6d64e.zip &&
+    if wget https://github.com/bepass-org/warp-plus/releases/download/v1.1.0/warp-plus-android-arm64.cdb551.zip &&
+        unzip warp-plus-android-arm64.cdb551.zip &&
+        mv warp-plus warp &&
         chmod +x warp &&
         cp warp "$PREFIX/bin/usef" &&
+        cp warp "$PREFIX/bin/warp-plus" &&
         cp warp "$PREFIX/bin/warp"; then
-        rm "README.md" "LICENSE" "warp-android-arm64.b6d64e.zip"
+        rm "README.md" "LICENSE" "warp-plus-android-arm64.cdb551.zip"
         echo "================================================"
         echo -e "${green}Warp installed successfully.${rest}"
         socks
@@ -104,15 +83,18 @@ install_arm() {
         *) echo -e "${red}Unsupported architecture.${rest}"; return ;;
     esac
 
-    WARP_URL="https://github.com/bepass-org/wireguard-go/releases/download/v1.0.4-rc2/warp-linux-$ARCH.b6d64e.zip"
+    WARP_URL="https://github.com/bepass-org/warp-plus/releases/download/v1.1.0/warp-plus-linux-$ARCH.cdb551.zip"
 
     if wget "$WARP_URL" &&
-        unzip "warp-linux-$ARCH.b6d64e.zip" &&
+        unzip "warp-plus-linux-$ARCH.cdb551.zip" &&
+        mv warp-plus warp &&
         chmod +x warp &&
         cp warp "$PREFIX/bin/usef" &&
+        cp warp "$PREFIX/bin/warp-plus" &&
         cp warp "$PREFIX/bin/warp"; then
-        rm "README.md" "LICENSE" "warp-linux-$ARCH.b6d64e.zip"
-        echo "================================================"
+        rm "README.md" "LICENSE" "warp-plus-linux-$ARCH.cdb551.zip"
+        echo -e "${blue}================================================${rest}"
+        echo -e "${blue}================================================${rest}"
         echo -e "${green}Warp installed successfully.${rest}"
         socks
     else
@@ -124,25 +106,128 @@ install_arm() {
 socks() {
    echo ""
    echo -e "${yellow}Copy this Config to ${purple}V2ray${green} Or ${purple}Nekobox ${yellow}and Exclude Termux${rest}"
-   echo "================================================"
+   echo ""
    echo -e "${green}socks://Og==@127.0.0.1:8086#warp_(usef)${rest}"
    echo "or"
    echo -e "${green}Manually create a SOCKS configuration with IP ${purple}127.0.0.1 ${green}and port${purple} 8086..${rest}"
-   echo "================================================"
-   echo -e "${yellow}To run again, type:${green} warp ${rest}or${green} usef ${rest}or${green} ./warp${rest}"
-   echo "================================================"
+   echo -e "${blue}================================================${rest}"
+   echo -e "${yellow}To run again, type:${green} warp ${rest}or${green} usef ${rest}or${green} ./warp ${rest}or${green} warp-plus ${rest}"
+   echo -e "${blue}================================================${rest}"
    echo -e "${green} If you get a 'Bad address' error, run ${yellow}[Arm]${rest}"
+   echo -e "${blue}================================================${rest}"
+   echo -e "${blue}================================================${rest}"
    echo ""
+}
+
+# Gool (warp in warp)
+gool() {
+    if ! command -v warp &> /dev/null || ! command -v usef &> /dev/null; then
+        echo -e "${purple}*********************************${rest}"
+        echo -e "${green}Please install Warp first.${rest}"
+        return
+    fi
+    echo -e "${purple}*********************************${rest}"
+    echo -e "${green}This option changes your current location to the nearest and best location.${rest}"
+    echo -e "${purple}*********************************${rest}"
+    warp --gool
+}
+
+# Psiphon
+psiphon_location() {
+    if ! command -v warp &> /dev/null || ! command -v usef &> /dev/null; then
+        echo -e "${purple}*********************************${rest}"
+        echo -e "${green}Please install Warp first.${rest}"
+        return
+    fi
+    echo -e "${purple}*********************************${rest}"
+    echo -e "${cyan}Please choose a location from the list below by entering its number:${rest}"
+    echo ""
+    echo -e "${purple}1)${yellow} Austria (AT)${rest}"
+    echo -e "${purple}2)${yellow} Belgium (BE)${rest}"
+    echo -e "${purple}3)${yellow} Bulgaria (BG)${rest}"
+    echo -e "${purple}4)${yellow} Brazil (BR)${rest}"
+    echo -e "${purple}5)${yellow} Canada (CA)${rest}"
+    echo -e "${purple}6)${yellow} Switzerland (CH)${rest}"
+    echo -e "${purple}7)${yellow} Czech Republic (CZ)${rest}"
+    echo -e "${purple}8)${yellow} Germany (DE)${rest}"
+    echo -e "${purple}9)${yellow} Denmark (DK)${rest}"
+    echo -e "${purple}10)${yellow} Estonia (EE)${rest}"
+    echo -e "${purple}11)${yellow} Spain (ES)${rest}"
+    echo -e "${purple}12)${yellow} Finland (FI)${rest}"
+    echo -e "${purple}13)${yellow} France (FR)${rest}"
+    echo -e "${purple}14)${yellow} United Kingdom (GB)${rest}"
+    echo -e "${purple}15)${yellow} Hungary (HU)${rest}"
+    echo -e "${purple}16)${yellow} Ireland (IE)${rest}"
+    echo -e "${purple}17)${yellow} India (IN)${rest}"
+    echo -e "${purple}18)${yellow} Italy (IT)${rest}"
+    echo -e "${purple}19)${yellow} Japan (JP)${rest}"
+    echo -e "${purple}20)${yellow} Latvia (LV)${rest}"
+    echo -e "${purple}21)${yellow} Netherlands (NL)${rest}"
+    echo -e "${purple}22)${yellow} Norway (NO)${rest}"
+    echo -e "${purple}23)${yellow} Poland (PL)${rest}"
+    echo -e "${purple}24)${yellow} Romania (RO)${rest}"
+    echo -e "${purple}25)${yellow} Serbia (RS)${rest}"
+    echo -e "${purple}26)${yellow} Sweden (SE)${rest}"
+    echo -e "${purple}27)${yellow} Singapore (SG)${rest}"
+    echo -e "${purple}28)${yellow} Slovakia (SK)${rest}"
+    echo -e "${purple}29)${yellow} Ukraine (UA)${rest}"
+    echo -e "${purple}30)${yellow} United States (US)${rest}"
+    echo ""
+
+    echo -en "${green}Enter the ${yellow}number${green} of the location [${yellow}default: 1${green}]: ${rest}"
+    choice=${choice:-AT}
+    read -r choice
+    
+    case "$choice" in
+        1) location="AT" ;;
+        2) location="BE" ;;
+        3) location="BG" ;;
+        4) location="BR" ;;
+        5) location="CA" ;;
+        6) location="CH" ;;
+        7) location="CZ" ;;
+        8) location="DE" ;;
+        9) location="DK" ;;
+        10) location="EE" ;;
+        11) location="ES" ;;
+        12) location="FI" ;;
+        13) location="FR" ;;
+        14) location="GB" ;;
+        15) location="HU" ;;
+        16) location="IE" ;;
+        17) location="IN" ;;
+        18) location="IT" ;;
+        19) location="JP" ;;
+        20) location="LV" ;;
+        21) location="NL" ;;
+        22) location="NO" ;;
+        23) location="PL" ;;
+        24) location="RO" ;;
+        25) location="RS" ;;
+        26) location="SE" ;;
+        27) location="SG" ;;
+        28) location="SK" ;;
+        29) location="UA" ;;
+        30) location="US" ;;
+        *) echo "Invalid choice. Please select a valid location number." ;;
+    esac
+
+    # Now use the selected location variable $location in your script
+    echo -e "${purple}*********************************${rest}"
+    echo -e "${green}Selected location: $location${rest}"
+    warp --cfon --country $location
 }
 
 #Uninstall
 uninstall() {
     warp="$PREFIX/bin/warp"
-    directory="/data/data/com.termux/files/home/wireguard-go"
+    directory="/data/data/com.termux/files/home/warp-plus"
     home="/data/data/com.termux/files/home"
     if [ -f "$warp" ]; then
-        rm -rf "$directory" "$PREFIX/bin/usef" "wa.py" "$PREFIX/bin/warp" "$home/wgcf-profile.ini" "$home/warp" "$home/stuff" "$home/wgcf-identity.json" > /dev/null 2>&1
+        rm -rf "$directory" "$PREFIX/bin/usef" "wa.py" "$PREFIX/bin/warp" "$PREFIX/bin/warp-plus" "warp" "stuff" > /dev/null 2>&1
+        echo -e "${purple}*********************************${rest}"
         echo -e "${red}Uninstallation completed.${rest}"
+        echo -e "${purple}*********************************${rest}"
     else
         echo -e "${yellow} ____________________________________${rest}"
         echo -e "${red} Not installed.Please Install First.${rest}${yellow}|"
@@ -167,27 +252,30 @@ menu() {
     clear
     echo -e "${green}By --> Peyman * Github.com/Ptechgithub * ${rest}"
     echo ""
-    echo -e "${yellow}❤️Github.com/${cyan}bepass-org${yellow}/wireguard-go❤️${rest}"
+    echo -e "${yellow}❤️Github.com/${cyan}bepass-org${yellow}/warp-plus❤️${rest}"
     echo -e "${purple}*********************************${rest}"
-    echo -e "${blue}     ###${cyan} Warp in Termux ${blue}###${rest}   ${purple}  * ${rest}"
+    echo -e "${blue}  ###${cyan} Warp-Plus in Termux ${blue}###${rest} ${purple}  * ${rest}"
     echo -e "${purple}*********************************${rest}"
-    echo -e "${cyan}1)${rest} ${green}Install Warp (vpn)${purple}           * ${rest}"
+    echo -e "${cyan}1]${rest} ${green}Install Warp (vpn)${purple}           * ${rest}"
     echo -e "                              ${purple}  * ${rest}"
-    echo -e "${cyan}2)${rest} ${green}Install Warp (vpn) [${yellow}Arm${green}] ${purple}    * ${rest}"
+    echo -e "${cyan}2]${rest} ${green}Install Warp (vpn) [${yellow}Arm${green}] ${purple}    * ${rest}"
     echo -e "                              ${purple}  * ${rest}"
-    echo -e "${cyan}3)${rest} ${green}Uninstall${rest}${purple}                    * ${rest}"
+    echo -e "${cyan}3]${rest} ${green}Uninstall${rest}${purple}                    * ${rest}"
     echo -e "                              ${purple}  * ${rest}"
-    echo -e "${cyan}4)${rest} ${green}Warp to ${purple}Warp plus${green} [${yellow}Free GB${green}]${rest}${purple}  * ${rest}"
+    echo -e "${cyan}4]${rest} ${green}Gool [${yellow}warp in warp${green}]${purple}          * ${rest}"
     echo -e "                              ${purple}  * ${rest}"
-    echo -e "${cyan}5)${rest} ${green}Build (warp)${purple}                 * ${rest}"
+    echo -e "${cyan}5]${rest} ${green}Pshiphon [${yellow}+ All Locations${green}]${purple}   * ${rest}"
     echo -e "                              ${purple}  * ${rest}"
-    echo -e "${red}0)${rest} ${green}Exit                         ${purple}* ${rest}"
+    echo -e "${cyan}6]${rest} ${green}Warp to ${purple}Warp plus${green} [${yellow}Free GB${green}]${rest}${purple}  * ${rest}"
+    echo -e "                              ${purple}  * ${rest}"
+    echo -e "${red}0]${rest} ${green}Exit                         ${purple}* ${rest}"
     echo -e "${purple}*********************************${rest}"
 }
 
 # Main
 menu
-read -p "Please enter your selection [0-5]:" choice
+echo -en "${cyan}Please enter your selection [${yellow}0-6${green}]:${rest}"
+read -r choice
 
 case "$choice" in
    1)
@@ -202,16 +290,22 @@ case "$choice" in
         uninstall
         ;;
     4)
-        warp_plus
+        gool
         ;;
     5)
-        build
+        psiphon_location
+        ;;
+    6)
+        warp_plus
         ;;
     0)
-        echo -e "${cyan}Exiting...${rest}"
+        echo -e "${purple}*********************************${rest}"
+        echo -e "${cyan}By 🖐${rest}"
         exit
         ;;
     *)
-        echo "Invalid choice. Please select a valid option."
+        echo -e "${purple}*********************************${rest}"
+        echo -e "${red}Invalid choice. Please select a valid option.${rest}"
+        echo -e "${purple}*********************************${rest}"
         ;;
 esac
